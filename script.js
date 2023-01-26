dfefb263b16813fe83c34db9158248ca11a86a89
cesarjakub <list>
2023-01-26 08:34:33 +0100

    feat: font

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const ctx = document.getElementById("canvas").getContext("2d");
+let barva = document.querySelector(".color");
+let tuzka = document.querySelector(".tuzka");
+let stetec = document.querySelector(".stetec");
+let vymazat = document.querySelector(".vymazat");
 
 function draw(){
-    
+    ctx.fillStyle = barva.value;
+    console.log(barva);
 }
 
 draw();
