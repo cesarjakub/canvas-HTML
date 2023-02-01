968e44b6819676f69370bf8cac2b6388a4a87149
cesarjakub <list>
2023-02-01 21:33:40 +0100

    feat: obdelnik

--- script.js
+++ script.js
@@ -4,20 +4,30 @@ let barva = document.querySelector(".color");
 let tuzka = document.querySelector(".tenkaTuzka");
 let stetec = document.querySelector(".tlustaTuzka");
 let vymazat = document.querySelector(".vymazat");
+let obdel = document.querySelector(".obdelnik");
 let x = 0;
 let y = 0;
 let nastroj = tuzka;
+let ob = false;
 
 document.addEventListener("mousedown", zacitKreslit);
 document.addEventListener("mouseup", prestatKreslit);
 
 function zacitKreslit(event) {
-  document.addEventListener("mousemove", draw);
-  x = event.clientX - canvas.offsetLeft;
-  y = event.clientY - canvas.offsetTop;
+  if(!ob){
+    document.addEventListener("mousemove", draw);
+    x = event.clientX - canvas.offsetLeft;
+    y = event.clientY - canvas.offsetTop;
+  }else{
+    document.addEventListener("mousemove", obdelnik);
+    x = event.clientX - canvas.offsetLeft;
+    y = event.clientY - canvas.offsetTop;
+  }
+  
 }
 function prestatKreslit() {
   document.removeEventListener("mousemove", draw);
+  document.removeEventListener("mousemove", obdelnik);
 }
 function draw(event) {
   ctx.beginPath();
@@ -31,6 +41,18 @@ function draw(event) {
   ctx.stroke();
 }
 
+function obdelnik(event){
+  ctx.beginPath();
+  ctx.lineCap = "round";
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = barva.value;
+  ctx.rect(x,y,100,50);
+  ctx.stroke();
+}
+obdel.addEventListener("click", () => {
+  ob = true;
+});
+
 function changeBrush() {
   switch (nastroj) {
     case tuzka:
@@ -41,9 +63,11 @@ function changeBrush() {
 }
 tuzka.addEventListener("click", () => {
   nastroj = tuzka;
+  ob = false;
 });
 stetec.addEventListener("click", () => {
   nastroj = stetec;
+  ob = false;
 });
 
 function reset() {
